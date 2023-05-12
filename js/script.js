79e05a61313f6b066d43eee83ee34612016fa1e8
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-05-12 17:08:33 +0000

    synic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Oct 2022
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-07-HTML/",
+  })
+}
+
+/**
+ * This function does multiplication.
+ */
+function myButtonClicked() {
+  var numA = parseFloat(document.getElementById("firstInputedNumber").value)
+  var product = 0
+  var counter = 0
+
+  while (counter <= numA) {
+    product = product + counter
+    counter++
+  }
+
+  document.getElementById("product").innerHTML = "The product is: " + product
+}
+Footer
